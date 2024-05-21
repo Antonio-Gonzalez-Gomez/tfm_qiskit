de53eb35063a746d0ae9a49ce3298eb2bdc30699
Antonio González Gómez <61906376+Antonio-Gonzalez-Gomez@users.noreply.github.com>
2024-05-21 21:47:16 +0200

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-for i in range(1, 16):
-    input = format(i, '04b')
-    uno_inds = [ind for ind in range(4) if input.startswith("1", ind)]
-    res_esperado = i * 7 % 15
-    print(i, "->",input, "* 0111 =", format(res_esperado, '04b'))
-
